2c7f6b108f3f3130f0a0d3d32c104d864fe13d18
Bernardo Nishikawa <bernardo@nishikawa.co>
2020-03-23 18:07:58 -0600

    Small Tweaks for NPM Usage

--- src/index.js
+++ src/index.js
@@ -1,2 +1,26 @@
-require = require("esm")(module/*, options*/)
-module.exports = require("./src/index")
+import Templates from './templates';
+import send from './send';
+import { getTransportClass } from './utlis';
+
+const Parrot = {
+  settings: {},
+  init(settings) {
+    const newSettings = {
+      ...{
+        defaultClass: 'email',
+      },
+      ...settings,
+      ...{
+        transports: settings.transports.map((t) => ({
+          ...t,
+          class: t.class || getTransportClass(t.name),
+        })),
+      },
+    };
+    this.settings = newSettings;
+  },
+  templates: new Templates(this),
+  send: (data, transport) => send(data, this.default.settings, transport),
+};
+
+export default Parrot;
