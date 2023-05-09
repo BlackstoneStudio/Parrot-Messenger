8cd216b7684359a20dd2e10e5aac964365e0f517
Jose Rodriguez <j.rodriguez@blackstone.studio>
2023-05-09 12:32:48 -0700

    FUNC [BLKS-1793] SES service
    Upgrade aws-sdk to V3

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,24 +1,24 @@
-import { ConfigurationOptions } from 'aws-sdk';
+import { SESClientConfig } from '@aws-sdk/client-ses';
 import * as SMTPTransport from 'nodemailer/lib/smtp-transport';
 
 export type Envelope = {
-	from?: string
-	to?: string
-	subject?: string
-	html?: string
-	attachment?: any | Array<any>
-	text?: string
-}
+  from?: string;
+  to?: string;
+  subject?: string;
+  html?: string;
+  attachment?: any | Array<any>;
+  text?: string;
+};
 export interface GenericTransport<T extends unknown = {}> {
-	transport: T
-	send(envelope: Envelope): Promise<void>
+  transport: T;
+  send(envelope: Envelope): Promise<void>;
 }
 
 export type Defaults = {
-	defaults?: Envelope
-}
+  defaults?: Envelope;
+};
 export interface AWSSESConfig extends Defaults {
-	auth: ConfigurationOptions
+  auth: SESClientConfig;
 }
 
 // export interface MailjetEmail extends Defaults {
@@ -33,71 +33,74 @@ export interface AWSSESConfig extends Defaults {
 // 	}
 // }
 export interface TwilioCall extends Defaults {
-	auth: {
-		sid: string
-		token: string
-	}
+  auth: {
+    sid: string;
+    token: string;
+  };
 }
 export interface TwilioSMS extends Defaults {
-	auth: {
-		sid: string
-		token: string
-	}
+  auth: {
+    sid: string;
+    token: string;
+  };
 }
 export interface Mailchimp extends Defaults {
-	auth: {
-		apiKey: string
-	}
+  auth: {
+    apiKey: string;
+  };
 }
 export interface Sendgrid extends Defaults {
-	auth: {
-		apiKey: string
-	}
+  auth: {
+    apiKey: string;
+  };
 }
 
 export interface Mailgun extends Defaults {
-	auth: {
-		apiKey: string;
-		domain: string;
-	}
+  auth: {
+    apiKey: string;
+    domain: string;
+  };
 }
 
 export interface SMTP extends Defaults {
-	auth: SMTPTransport.Options
+  auth: SMTPTransport.Options;
 }
 
 interface TransportGeneric<
-	N extends string,
-	C extends 'email'|'sms'|'call',
-	S extends {}
+  N extends string,
+  C extends 'email' | 'sms' | 'call',
+  S extends {}
 > {
-	name: N,
-	class: C,
-	settings: S
+  name: N;
+  class: C;
+  settings: S;
 }
 
 export type Transport =
-	| TransportGeneric<'ses', 'email', AWSSESConfig>
-	// | TransportGeneric<'mailjetEmail', 'email', MailjetEmail>
-	// | TransportGeneric<'mailjetSMS', 'sms', MailjetSMS>
-	| TransportGeneric<'twilioSMS', 'sms', TwilioSMS>
-	| TransportGeneric<'twilioCall', 'call', TwilioCall>
-	| TransportGeneric<'mailchimp', 'email', Mailchimp>
-	| TransportGeneric<'mailgun', 'email', Mailgun>
-	| TransportGeneric<'sendgrid', 'email', Sendgrid>
-	| TransportGeneric<'smtp', 'email', SMTP>
+  | TransportGeneric<'ses', 'email', AWSSESConfig>
+  // | TransportGeneric<'mailjetEmail', 'email', MailjetEmail>
+  // | TransportGeneric<'mailjetSMS', 'sms', MailjetSMS>
+  | TransportGeneric<'twilioSMS', 'sms', TwilioSMS>
+  | TransportGeneric<'twilioCall', 'call', TwilioCall>
+  | TransportGeneric<'mailchimp', 'email', Mailchimp>
+  | TransportGeneric<'mailgun', 'email', Mailgun>
+  | TransportGeneric<'sendgrid', 'email', Sendgrid>
+  | TransportGeneric<'smtp', 'email', SMTP>;
 
 export type Settings = {
-	defaultClass?: string
-	transports: Transport[]
-}
+  defaultClass?: string;
+  transports: Transport[];
+};
 
 export type ParrotSettings = {
-	defaultClass?: string
-	transports: Omit<Transport, 'class'>[]
-}
+  defaultClass?: string;
+  transports: Omit<Transport, 'class'>[];
+};
 
 export interface Mailer<T> {
-	send(message: Envelope, transport?: Omit<Transport, 'settings'>|Omit<Transport, 'settings'>[]): void
-	templates: T
+  send(
+    message: Envelope,
+    transport?: Omit<Transport, 'settings'> | Omit<Transport, 'settings'>[]
+  ): void;
+  templates: T;
 }
